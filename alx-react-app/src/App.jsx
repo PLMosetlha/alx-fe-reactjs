e106edb040e556e7825235580cb8cb7d46da97fd
Precious Mosetlha <caseymosetlha@gmail.com>
2025-02-16 19:14:31 +0200

    third commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,17 @@
 import WelcomeMessage from "./WelcomeMessage";
+import Header from "./Header.jsx";
+import MainContent from "./MainContent.jsx";
+import Footer from "./Footer.jsx";
+
 function App() {
-  return <WelcomeMessage />;
+  return (
+    <>
+      <WelcomeMessage />;
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
