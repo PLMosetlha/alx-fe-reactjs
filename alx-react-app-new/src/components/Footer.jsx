71837321440c67d27e5af6732e7168b0a0cdff90
PLMosetlha <caseymosetlha@gmail.com>
2025-02-23 15:47:21 +0200

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer>
-      <p>© 2023 City Lovers</p>
+    <footer  style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+      <p style={{ color: 'blue' }} >© 2023 City Lovers</p>
     </footer>
   );
 }
