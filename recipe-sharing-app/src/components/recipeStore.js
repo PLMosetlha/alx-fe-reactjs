144fc50b94eb407de67ee23d12a5dfbe57b6a82a
Precious Mosetlha <caseymosetlha@gmail.com>
2025-02-25 05:09:26 +0200

    first commit

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,17 @@ import create from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',  // To store the search term
+  setSearchTerm: (term) => set({ searchTerm: term }),  // Action to set the search term
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()) ||
+      recipe.description.toLowerCase().includes(state.searchTerm.toLowerCase()) // Filter by title or description
+    )
+  })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
