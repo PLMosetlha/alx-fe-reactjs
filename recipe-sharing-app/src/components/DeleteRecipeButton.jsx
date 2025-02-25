cbc6da24f2053a74efd02b5f63974ca8fadb5833
Precious Mosetlha <caseymosetlha@gmail.com>
2025-02-25 04:10:08 +0200

    fourth commit

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,9 +1,13 @@
-import useRecipeStore from "../store/recipeStore";
+import { useRecipeStore } from "../recipeStore";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
-  return <button onClick={() => deleteRecipe(recipeId)}>Delete Recipe</button>;
+  const handleDelete = () => {
+    deleteRecipe(recipeId);
+  };
+
+  return <button onClick={handleDelete}>Delete Recipe</button>;
 };
 
 export default DeleteRecipeButton;
