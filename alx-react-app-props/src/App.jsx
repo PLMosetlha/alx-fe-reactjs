9f4707c6718f6542f92c107e5b1c7911d5a761eb
Precious Mosetlha <caseymosetlha@gmail.com>
2025-02-25 03:17:02 +0200

    second commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,14 @@
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
-import UserProfile from "./UserProfile.jsx";
-import WelcomeMessage from "./WelcomeMessage.jsx";
-import Counter from "./Counter.jsx";
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <WelcomeMessage />
-      <Counter />
-    </>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
+
+export default App;
