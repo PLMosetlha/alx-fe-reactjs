286a0ade37227f37c9efe03abf530d5c523bedce
Precious Mosetlha <caseymosetlha@gmail.com>
2025-03-07 13:18:01 +0200

    third commit

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,16 @@
 import FormikForm from "./formikForm.js";
-import RegistrationForm from "./RegistrationForm.jsx";
+import RegistrationForm from "./RegistrationForm.js";
 function App() {
   return (
     <div>
       <h1>User Registration</h1>
+
+      {/* Controlled Component Form */}
+      <h2>Controlled Form</h2>
       <RegistrationForm />
+
+      {/* Formik Form */}
+      <h2>Formik Form</h2>
       <FormikForm />
     </div>
   );
