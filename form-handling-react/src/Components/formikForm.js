a8465a6befe9e09bff07bed4780610a881712a3a
Precious Mosetlha <caseymosetlha@gmail.com>
2025-03-07 12:54:05 +0200

    first commit

---
+++ form-handling-react/src/Components/formikForm.js
@@ -0,0 +1,41 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
+
+const FormikForm = () => {
+  return (
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        console.log("Form submitted", values);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="text" name="username" placeholder="Username" />
+          <ErrorMessage name="username" component="p" />
+
+          <Field type="email" name="email" placeholder="Email" />
+          <ErrorMessage name="email" component="p" />
+
+          <Field type="password" name="password" placeholder="Password" />
+          <ErrorMessage name="password" component="p" />
+
+          <button type="submit" disabled={isSubmitting}>
+            Register
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default FormikForm;
