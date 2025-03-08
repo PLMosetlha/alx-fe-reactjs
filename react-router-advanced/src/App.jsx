149ad67bc4218f5a7245307e2182ee6190b3da14
Precious Mosetlha <caseymosetlha@gmail.com>
2025-03-08 04:38:06 +0200

    first commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./Profile";
 import NotFound from "./NotFound";
 import Login from "./Login";
 import BlogPost from "./components/BlogPost"; // Import BlogPost component
+import ProtectedRoute from "./components/ProtectedRoute"; // Import the ProtectedRoute
 
 function App() {
   return (
@@ -17,6 +18,12 @@ function App() {
         <Route path="/post/:id" element={<Post />} />
         <Route path="/blog/:id" element={<BlogPost />} />{" "}
         {/* Dynamic routing for blog post */}
+        <Route path="/" element={<Home />} />
+        {/* Use ProtectedRoute for the Profile page */}
+        <Route
+          path="/profile"
+          element={<ProtectedRoute element={<Profile />} />}
+        />
       </Routes>
     </Router>
   );
