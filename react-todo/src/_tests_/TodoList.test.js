e20a86aeea2fd35d9fe5674f862817666ed77d9d
Precious Mosetlha <caseymosetlha@gmail.com>
2025-03-09 20:00:04 +0200

    second commit

---
+++ react-todo/src/_tests_/TodoList.test.js
@@ -0,0 +1,36 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+test("renders initial todo items", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a project")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText("Add a new todo");
+  const addButton = screen.getByText("Add");
+
+  fireEvent.change(input, { target: { value: "Test new todo" } });
+  fireEvent.click(addButton);
+
+  expect(screen.getByText("Test new todo")).toBeInTheDocument();
+});
+
+test("toggles todo completion", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo item", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+  const deleteButton = todoItem.nextSibling;
+
+  fireEvent.click(deleteButton);
+  expect(todoItem).not.toBeInTheDocument();
+});
