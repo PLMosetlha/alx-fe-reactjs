11e1005db77dae6ea960a81e6c64706a92a20362
Precious Mosetlha <caseymosetlha@gmail.com>
2025-03-30 23:11:39 +0200

    2nd project

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import UserProfile from "./UserProfile";
+import githubApi from "./githubApi";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const data = await githubApi.getUserData(username);
+      setUserData(data);
+    } catch (error) {
+      console.error("Error searching for user:", error);
+    }
+  };
+
   return (
     <div className="App">
       <h1>GitHub User Search</h1>
-      <input type="text" placeholder="Search for a GitHub user" />
-      {/* Add functionality to display user information here */}
+      <SearchBar
+        username={username}
+        setUsername={setUsername}
+        onSearch={handleSearch}
+      />
+      <UserProfile userData={userData} />
     </div>
   );
 }
